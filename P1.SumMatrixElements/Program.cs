ced9a59b9ca52326a5a381b462fbba60dc1f96b6
I-M-Marinov <lcfrrr@gmail.com>
2023-09-15 15:10:18 -0500

    Added a few problems to be solved
    Added a few problems to be solved

--- P1.SumMatrixElements/Program.cs
+++ P1.SumMatrixElements/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+
+int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+int rows = sizes[0];
+int cols = sizes[1];
+
+int[,] matrix = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] rowValues = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        
+    }
+}
+
+
